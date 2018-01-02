5cb12ac139275f56c4b34bddbc1c7bd5e81bba83
Sima Margulis Davidi <simamarg@gmail.com>
2018-01-02 12:10:10 +0200

    add search by book title and chosing a book

--- script.js
+++ script.js
@@ -1,23 +1,30 @@
 function displayBook(data) {
-    var dataPath = data.items[0].volumeInfo;
-    var dataItems = {
-        title: `${dataPath.title}`,
-        description: `${dataPath.description}`,
-        author: `${dataPath.authors}`,
-        image: `${dataPath.imageLinks.smallThumbnail}`
-    };
+    var dataItems = data.items;
+    var booksObject = {books: []};
+    for (i=0; i < dataItems.length; i++) {
+        var dataPath = dataItems[i].volumeInfo;
+        var bookObject = {
+            id: i,
+            title: dataPath.title,
+            description: dataPath.description,
+            author: dataPath.authors,
+            image: dataPath.imageLinks.smallThumbnail
+        };
+        booksObject.books.push(bookObject);
+    }
     
     var source = $('#book-template').html();
     var template = Handlebars.compile(source);
-    var newHTML = template(dataItems);
-    $('.book').append(newHTML);
+    var newHTML = template(booksObject);
+    $('.books').append(newHTML);
 }
 
-var fetch = function (isbn) {
+var fetch = function (title) {
     $.ajax({
         method: "GET",
-        url: 'https://www.googleapis.com/books/v1/volumes?q=isbn:' + isbn,
+        url: 'https://www.googleapis.com/books/v1/volumes?q=intitle:' + title,
         success: function (data) {
+            console.log(data);
             displayBook(data);
         },
         error: function (jqXHR, textStatus, errorThrown) {
@@ -26,10 +33,14 @@ var fetch = function (isbn) {
     });
 };
 
-// isbn: 0439023521
-
 $('button').on('click', function() {
-    var isbn = $(this).closest('form').find('input').val();
-    fetch(isbn);
+    var userInput = $(this).closest('form').find('input').val();
+    fetch(userInput);
     $('input').val('');
+});
+
+$('.books').on('click', '.book', function() {
+    $clickedBook = $(this);
+    $('.books').empty();
+    $('.books').append($clickedBook);
 });
